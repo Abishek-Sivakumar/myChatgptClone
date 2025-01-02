924d7287ad297619bfad683e36d8087857419ea4
ABISHEK <abideepu19.vrs@gmail.com>
2025-01-02 23:33:57 +0530

    Getting input from user
    There is a bug where the initial getMessage returns an error since there is an empty array passed to the ai.

--- myChatgptClone/src/App.jsx
+++ myChatgptClone/src/App.jsx
@@ -3,30 +3,46 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import getMessageFromMistral from "./ai.js";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [messages, setMessages] = useState([
-    "This is the initial Message",
-    "This is the second message",
-  ]);
+  const [messageFromUser, setMessageFromUser] = useState([]);
 
-  const messagesChat = messages.map((msg) => {
+  const [messageFromBot, setMessageFromBot] = useState([]);
+
+  // useEffect(() => {
+  //   setMessageFromUser((initialMsg) => [...initialMsg, "Hello Mistral"]);
+  // });
+
+  const messagesChat = messageFromBot.map((msg) => {
     return (
-      <div className="message-div">
+      <div className="message-div" key={nanoid()}>
         <h2>{msg}</h2>
       </div>
     );
   });
 
   async function getMessage() {
-    const msgdata = await getMessageFromMistral(messages[messages.length - 1]);
-    setMessages((prevMsg) => [...prevMsg, msgdata]);
-    console.log(msgdata);
+    setMessageFromUser((prevUserMsg) => [
+      ...prevUserMsg,
+      document.getElementById("userInput").value,
+    ]);
+    console.log(messageFromUser);
+    const msgData = await getMessageFromMistral(
+      messageFromUser[messageFromUser.length - 1]
+    );
+    setMessageFromBot((prevBotMsg) => [...prevBotMsg, msgData]);
+    console.log(msgData);
   }
 
   return (
     <>
       <section className="messages-container">{messagesChat}</section>
+      <input
+        type="text"
+        id="userInput"
+        placeholder="How Can I help You Today"
+      ></input>
       <button className="sendButton" onClick={getMessage}>
         Send Message
       </button>
